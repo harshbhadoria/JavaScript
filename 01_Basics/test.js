8b13a00e4395d32cbfb21633fed448e1673bbb85
Harsh Bhadoria <jatinshown11@gmail.com>
2024-08-09 17:44:02 +0000

    09 done

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,10 @@
-console.log("Hii, Harsh Bhadoria ")
+// 09-08-2024
+
+// console.log("Hii, Harsh Bhadoria ")
+// AccId = null
+// AccNumber = 989898
+// AccCity = "Bombay"
+// // console.log(typeof AccId)
+// console.table([ AccCity, AccId, AccNumber])
+
+// 10-08-2024 -- To be Continue
