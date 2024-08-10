8ee99de7d4f684abd615dc56beab6a4d612875c7
Harsh Bhadoria <jatinshown11@gmail.com>
2024-08-10 17:21:47 +0000

    Conversion of Datatypres

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -8,3 +8,22 @@
 // console.table([ AccCity, AccId, AccNumber])
 
 // 10-08-2024 -- To be Continue
+
+// let score = NaN
+
+// console.log(typeof score);
+// let ConvertNum = Number(score)
+// console.log(typeof ConvertNum);
+// console.log( ConvertNum);
+
+let isloggedIn = 0.1
+
+console.log(typeof isloggedIn);
+
+
+// let boolIsLoggedIn  = Boolean(isloggedIn)
+let boolIsLoggedIn  = String(isloggedIn)
+console.log(boolIsLoggedIn);
+console.log(typeof boolIsLoggedIn);
+
+
