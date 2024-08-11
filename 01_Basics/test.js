28e8041243e3b85b6ce48db7d5857431a5012f15
Harsh Bhadoria <jatinshown11@gmail.com>
2024-08-11 18:18:32 +0000

    Operation

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -16,15 +16,36 @@
 // console.log(typeof ConvertNum);
 // console.log( ConvertNum);
 
-let isloggedIn = 0.1
+// let isloggedIn = 0.1
 
-console.log(typeof isloggedIn);
+// console.log(typeof isloggedIn);
 
 
-// let boolIsLoggedIn  = Boolean(isloggedIn)
-let boolIsLoggedIn  = String(isloggedIn)
-console.log(boolIsLoggedIn);
-console.log(typeof boolIsLoggedIn);
+// // let boolIsLoggedIn  = Boolean(isloggedIn)
+// let boolIsLoggedIn  = String(isloggedIn)
+// console.log(boolIsLoggedIn);
+// console.log(typeof boolIsLoggedIn);
 
 // 11-08-2024 -- To be Continue...
+
+let FName = "Harsh "
+let LName = "Bhadoria"
+
+FullName = FName + LName
+// console.log(FullName);
+
+// FullName = FName * LName
+// console.log(1 + 2 + "3");
+// console.log(true + 2.56 + 0.132);
+// console.log(+3+4);
+
+n1 = n2 = n3 = 100
+
+console.log(n1);
+console.log(n1++);
+console.log(++n1);
+console.log(n1--);
+console.log(--n1);
+
+// 12-08-2024 -- To be Continue...
 
