247be5855be7c2617cfbbbb1452551205ca19652
Harsh Bhadoria <jatinshown11@gmail.com>
2024-08-17 18:17:17 +0000

    maths and numbers

---
+++ JavaScript/nums_and_math.js
@@ -0,0 +1,52 @@
+// Numbers and Math in JavaScript
+
+// +++++++++++++++++++++++++++++ Numbers +++++++++++++++++++++++++++++
+
+const Num1 = 433.87
+// console.log(Num1);
+
+const Num2 = new Number(100)
+// console.log(Num2);
+
+// console.log(Num1.toPrecision(3));
+// console.log(Num2.toPrecision(2));
+
+// console.log(Num1.toString().split('.'));
+
+const Num3 = 3454987654
+// console.log(Num3.toLocaleString('en-IN'));
+// console.log(Num3.toFixed(3));
+
+// +++++++++++++++++++++++++++++ Math +++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-43));
+// console.log(Math.round(-54.53));
+// console.log(Math.sqrt(45));
+
+// console.log(Math.min(3,5,2,4,-4));
+// console.log(Math.pow(3,3));
+
+// console.log(((Math.random()*10)+1))
+// console.log(((Math.random()*10)+1).toFixed(0))
+// console.log(((Math.random()*10)+1).toFixed(0))
+// console.log(((Math.random()*10)+1).toFixed(0))
+// console.log(((Math.random()*6)+1).toFixed(0))
+// console.log(((Math.random()*6)+1).toFixed(0))
+// console.log(((Math.random()*6)+1).toFixed(0))
+// console.log(((Math.random()*6)+1).toFixed(0))
+// console.log(((Math.random()*6)+1).toFixed(0))
+// console.log(((Math.random()*6)+1).toFixed(0))
+
+// console.log(Math.floor(Math.random()*10)+1)
+
+min = 1
+max = 20
+
+console.log(Math.floor(Math.random() ))
+console.log(Math.floor(Math.random() + min))
+console.log(Math.floor(Math.random() * (max - min +1)) )
+console.log(Math.floor(Math.random() * (max - min +1)) + min)
+
+
+
+
