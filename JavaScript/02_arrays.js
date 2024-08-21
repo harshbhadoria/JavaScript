92837a02228862f65ff85f8059898e1b8254eb65
Harsh Bhadoria <jatinshown11@gmail.com>
2024-08-21 17:47:32 +0000

    02_arrays

---
+++ JavaScript/02_arrays.js
@@ -0,0 +1,28 @@
+const marvel_heros = ["Spiderman", "Loki", "Thanos"]
+const dc_heros = ["Batman", "Joker", "Superman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+
+// const newArr = marvel_heros.concat(dc_heros)
+// console.log(newArr);
+
+// const Combine_heros = [...marvel_heros, ...dc_heros]
+// console.log(Combine_heros);
+
+// const newArr = [1,2,3,[4,5,6],7,8,[6,7,[3,4,5]]]
+// console.log(newArr);
+
+
+// console.log(newArr.flat(3));
+
+
+// console.log(Array.isArray("Harsh bhadoria"));
+// console.log(Array.from("Harsh bhadoria"));
+// console.log(Array.from({name:"Harsh bhadoria", age : "23"}));
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
