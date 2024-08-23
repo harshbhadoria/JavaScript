40d207629aec6e70960ed1091822c1298a7e4798
Harsh Bhadoria <jatinshown11@gmail.com>
2024-08-23 16:48:10 +0000

    scopes in js

---
+++ JavaScript/scopes.js
@@ -0,0 +1,65 @@
+// let a = 10
+// const b = 20
+// var c =30
+
+let a =100 
+
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log(a);
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "Harsh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    
+    two()
+}
+
+// one()
+
+
+if(true){
+    const username = "Harsh"
+    if(username == "Harsh"){
+        const website = " Youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);    
+}
+// console.log(username);
+
+
+// +++++++++++++++++++++ intersting +++++++++++++++++++++
+
+
+function addone(num){
+    return ++num
+}
+
+console.log(addone(5));
+
+const addtwo = function(num){
+    return num + 2
+}
+
+// addtwo(7)
+
+
+
+
+
+
+
+
+
