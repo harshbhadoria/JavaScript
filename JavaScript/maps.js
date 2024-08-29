45a7ccce0312ac2897f9c6d209ed738fb8903a18
Harsh Bhadoria <jatinshown11@gmail.com>
2024-08-29 13:20:55 +0000

    maps 2

--- JavaScript/maps.js
+++ JavaScript/maps.js
@@ -26,11 +26,109 @@ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // console.log(newNums);
 // 
 
-const book = [
-    {
+// const book = [
+//     {
         
-    }
+//     }
+// ]
+
+
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums2 = myNumbers.map( (num) => num+10 )
+// console.log(newNums2);
+
+
+const newNums2 = myNumbers
+                    .map( (num) => num * 10)
+                    .map( (num) => num + 2)
+                    .filter( ( num ) => num%2 == 0)
+
+// console.log(newNums2);
+
+
+const array1  = [1, 2, 3]
+
+const flag = 0
+
+// const myTotal = array1.reduce(function (acc, currval) {
+//     console.log((`acc: ${acc} and currval: ${currval}`));
+//     return acc + currval
+// }, flag )
+
+const myTotal = array1.reduce( (acc, currval) => acc + currval, flag )
+
+
+// console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName : "Js Course",
+        price : 2999
+    },
+    {
+        itemName : "Mobile Development Course",
+        price : 12990
+    },
+    {
+        itemName : "React Course",
+        price : 5499
+    },
+    {
+        itemName : "SAP Course",
+        price : 25999
+    },
 ]
+
+
+const priceToPay = shoppingCart.reduce( (acc,item) => acc + item.price, 0 )
+
+
+console.log(priceToPay);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
