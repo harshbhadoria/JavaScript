c0f2c3127a4b6b4793092d7508edefbad16e4da2
Harsh Bhadoria <jatinshown11@gmail.com>
2024-09-12 17:16:34 +0000

    OOPs

---
+++ Advance_JS/Oops/notes.md
@@ -0,0 +1,24 @@
+# javascript and classes
+
+## OOPs
+
+## Objects
+- collection of properties and methods
+- toLowerCase
+
+## why use OOPs
+
+# 4 pillars of OOPs
+- Abstraction
+- Encapsultaion
+- Inheritance
+- Polymorphism
+
+## parts of OOPs
+- Object literals
+
+-- Constructor function
+-- Prototypes
+-- Classes
+-- Instances (new, this)
+
---
+++ Advance_JS/Oops/oop.js
@@ -0,0 +1,34 @@
+const user = {
+    username : "Harsh",
+    loginCount : 8,
+    signedIn : true,
+
+    getUserDetails : function (data) {
+        console.log(`Got user Details from database ${data}`);
+        console.log(`Got user Details from database ${this.username}`);
+        console.log(this);
+
+    }
+}
+
+// console.log(user.username);
+// console.log(user.getUserDetails("From Kalyug"));
+// console.log(this);
+
+function User(username, loginCount, isLoggedIn) {
+    this.username = username
+    this.loginCount = loginCount
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+    }
+
+    return this
+}
+
+const userOne = new User("harsh","12",true)
+const userTwo = new User("RajuNaan",11,false)
+
+console.log(userOne.constructor)
+console.log(userTwo.constructor)
