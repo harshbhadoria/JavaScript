a8489f101faf75af39818f2db2b72c7353d27c9c
Harsh Bhadoria <jatinshown11@gmail.com>
2024-09-19 15:10:47 +0000

    To Do complete

--- JS_Project/07_to_do_list/to_do.js
+++ JS_Project/07_to_do_list/to_do.js
@@ -1,24 +1,15 @@
-document.getElementById('add-task-btn').addEventListener('click', addtask)
+const todoList = document.getElementById('task-list');
+const newTaskInput = document.getElementById('task-input');
+const addTaskButton = document.getElementById('add-task-btn');
 
-function addtask(){
-    const taskInput = document.getElementById('task-input')
-    const taskText = taskInput.value.trim();
+addTaskButton.addEventListener('click', addTask);
 
-    if(taskText === ''){
-        alert('Please enter a task')
-        return
-    }
-
-    const taskList = document.getElementById('task-list')
-    const li = document.createElement('li')
-
-    li.textContent = taskText
-    li.addEventListener('click', toogleComplete)
-    taskList.appendChild(li)
-
-    taskInput.value = ''
-}
-
-function toogleComplete(){
-    event.target.classList.toogle('Completed!')
+function addTask() {
+  const taskText = newTaskInput.value.trim();
+  if (taskText) {
+    const taskListItem = document.createElement('li');
+    taskListItem.textContent = taskText;
+    todoList.appendChild(taskListItem);
+    newTaskInput.value = '';
+  }
 }
