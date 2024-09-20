4e091bf930d21f54b3efda9a49c225d507971ff3
Harsh Bhadoria <jatinshown11@gmail.com>
2024-09-20 15:44:16 +0000

    I don't Know!!

--- JS_Project/07_to_do_list/to_do.js
+++ JS_Project/07_to_do_list/to_do.js
@@ -1,15 +1,16 @@
-const todoList = document.getElementById('task-list');
-const newTaskInput = document.getElementById('task-input');
-const addTaskButton = document.getElementById('add-task-btn');
+// const todoList = document.getElementById('task-list');
+// const newTaskInput = document.getElementById('task-input');
+// const addTaskButton = document.getElementById('add-task-btn');
 
-addTaskButton.addEventListener('click', addTask);
+// addTaskButton.addEventListener('click', addTask);
 
-function addTask() {
-  const taskText = newTaskInput.value.trim();
-  if (taskText) {
-    const taskListItem = document.createElement('li');
-    taskListItem.textContent = taskText;
-    todoList.appendChild(taskListItem);
-    newTaskInput.value = '';
-  }
-}
+// function addTask() {
+//   const taskText = newTaskInput.value.trim();
+//   if (taskText) {
+//     const taskListItem = document.createElement('li');
+//     taskListItem.textContent = taskText;
+//     todoList.appendChild(taskListItem);
+//     newTaskInput.value = '';
+//   }
+// }
+
